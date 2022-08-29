a9467586fbe7d31c44f9b3b1743f37be61dcfcf4
zeropotential <mr.painini@gmail.com>
2022-08-29 23:15:53 +0600

    Created diffcallback

--- app/src/main/java/com/example/shoppingapp/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/shoppingapp/presentation/ShopListAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppingapp.R
 import com.example.shoppingapp.domain.ShopItem
@@ -14,18 +15,20 @@ import java.lang.RuntimeException
 class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
 
     var count = 0
-    var shopList = listOf<ShopItem>()
 
+    var shopList = listOf<ShopItem>()
     set(value) {
+        val callback = ShopListDiffCallback(shopList, value)
+        val diffResult = DiffUtil.calculateDiff(callback)
+        diffResult.dispatchUpdatesTo(this)
         field = value
-        notifyDataSetChanged()
     }
 
     var onShopItemLongClickListener: ((ShopItem) -> Unit)? = null
     var onShopItemClickListener: ((ShopItem) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        Log.d("ShopListAdapter", "On create view holder, count: ${count++}" )
+
         val layout = when (viewType){
             VIEW_TYPE_ENABLED -> R.layout.item_shop_enabled
             VIEW_TYPE_DISABLED -> R.layout.item_shop_disabled
@@ -38,6 +41,7 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
 
 
     override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
+        Log.d("ShopListAdapter", "OnBindViewHolder, count: ${count++}" )
         val shopItem = shopList[position]
 
         viewHolder.view.setOnLongClickListener {
